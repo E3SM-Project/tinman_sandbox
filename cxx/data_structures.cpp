7b26399e4f7a2a72d28e455b3efc6034bf10391e
Luca Bertagna <lbertag@sandia.gov>
2016-12-06 17:49:32 -0700

    Keep adding needed arrays/constants to the data_structure header

--- cxx/data_structures.cpp
+++ cxx/data_structures.cpp
@@ -1,49 +1,47 @@
 #include <cstdlib>
 
-#include "test_macros.hpp"
-
+#include "dimensions.hpp"
 #include "data_structures.hpp"
+
+namespace Homme
+{
 
 int init_test_data (TestData& data)
 {
-  constexpr int np          = NUM_POINTS;
-  constexpr int nc          = NUM_CORNERS;
-  constexpr int nlev        = NUM_LEVELS;
-  constexpr int nlevp       = NUM_LEVELS_P;
-  constexpr int nelems      = NUM_ELEMENTS;
-  constexpr int timelevels  = NUM_TIME_LEVELS;
-  constexpr int qsize_d     = QSIZE_D;
+  data.arrays.elem_D                    = new real[nelems*np*np*2*2];
+  data.arrays.elem_Dinv                 = new real[nelems*np*np*2*2];
+  data.arrays.elem_fcor                 = new real[nelems*np*np];
+  data.arrays.elem_spheremp             = new real[nelems*np*np];
+  data.arrays.elem_metdet               = new real[nelems*np*np];
+  data.arrays.elem_rmetdet              = new real[nelems*np*np];
+  data.arrays.elem_sub_elem_mass_flux   = new real[nlev*nc*nc*4];
 
-  data.arrays.elem_Dinv                 = new real[np*np*2*2*nelems];
-  data.arrays.elem_fcor                 = new real[np*np*nelems];
-  data.arrays.elem_spheremp             = new real[np*np*nelems];
-  data.arrays.elem_metdet               = new real[np*np*nelems];
-  data.arrays.elem_sub_elem_mass_flux   = new real[nc*nc*4*nlev];
+  data.arrays.elem_state_ps_v           = new real[nelems*timelevels*np*np];
+  data.arrays.elem_state_dp3d           = new real[nelems*timelevels*np*np*nlev];
+  data.arrays.elem_state_v              = new real[nelems*timelevels*np*np*2*nlev];
+  data.arrays.elem_state_T              = new real[nelems*timelevels*np*np*nlev];
+  data.arrays.elem_state_phis           = new real[nelems*np*np];
+  data.arrays.elem_state_Qdp            = new real[nelems*np*np*nlev*qsize_d*2];
 
-  data.arrays.elem_state_ps_v           = new real[np*np*timelevels*nelems];
-  data.arrays.elem_state_dp3d           = new real[np*np*nlev*timelevels*nelems];
-  data.arrays.elem_state_v              = new real[np*np*2*nlev*timelevels*nelems];
-  data.arrays.elem_state_T              = new real[np*np*nlev*timelevels*nelems];
-  data.arrays.elem_state_phis           = new real[np*np*nelems];
-  data.arrays.elem_state_Qdp            = new real[np*np*nlev*qsize_d*2*nelems];
-
-  data.arrays.elem_derived_eta_dot_dpdn = new real[np*np*nlevp*nelems];
-  data.arrays.elem_derived_omega_p      = new real[np*np*nlev*nelems];
-  data.arrays.elem_derived_phi          = new real[np*np*nlev*nelems];
-  data.arrays.elem_derived_pecnd        = new real[np*np*nlev*nelems];
-  data.arrays.elem_derived_ps_met       = new real[np*np*nelems];
-  data.arrays.elem_derived_dpsdt_met    = new real[np*np*nelems];
-  data.arrays.elem_derived_vn0          = new real[np*np*2*nlev*nelems];
+  data.arrays.elem_derived_eta_dot_dpdn = new real[nelems*np*np*nlevp];
+  data.arrays.elem_derived_omega_p      = new real[nelems*nlev*np*np];
+  data.arrays.elem_derived_phi          = new real[nelems*nlev*np*np];
+  data.arrays.elem_derived_pecnd        = new real[nelems*nlev*np*np];
+  data.arrays.elem_derived_ps_met       = new real[nelems*np*np];
+  data.arrays.elem_derived_dpsdt_met    = new real[nelems*np*np];
+  data.arrays.elem_derived_vn0          = new real[nelems*nlev*np*np*2];
 
   return EXIT_SUCCESS;
 }
 
 int cleanup_data (TestData& data)
 {
+  delete[] data.arrays.elem_D;
   delete[] data.arrays.elem_Dinv;
   delete[] data.arrays.elem_fcor;
   delete[] data.arrays.elem_spheremp;
   delete[] data.arrays.elem_metdet;
+  delete[] data.arrays.elem_rmetdet;
   delete[] data.arrays.elem_sub_elem_mass_flux;
 
   delete[] data.arrays.elem_state_ps_v;
@@ -63,3 +61,5 @@ int cleanup_data (TestData& data)
 
   return EXIT_SUCCESS;
 }
+
+} // Namespace Homme
--- cxx/data_structures.hpp
+++ cxx/data_structures.hpp
@@ -1,15 +1,37 @@
 #ifndef DATA_STRUCTURES_HPP
 #define DATA_STRUCTURES_HPP
 
-#include "test_macros.hpp"
+#include "kinds.hpp"
+#include "dimensions.hpp"
+
+namespace Homme
+{
+
+struct HVCoord
+{
+  real ps0;         // base state surface-pressure for level definitions
+  real hyai[nlevp]; // ps0 component of hybrid coordinate - interfaces
+  real hyam[nlev];  // ps0 component of hybrid coordinate - midpoints
+  real hybi[nlevp]; // ps  component of hybrid coordinate - interfaces
+  real hybm[nlev];  // ps  component of hybrid coordinate - midpoints
+  real hybd[nlev];  // difference in b (hybi) across layers
+  real prsfac;      // log pressure extrapolation factor (time, space independent)
+  real etam[nlev];  // eta-levels at midpoints
+  real etai[nlevp]; // eta-levels at interfaces
+
+  int nprlev;       // number of pure pressure levels at top
+  int pad;
+};
 
 struct Arrays
 {
   // Members of elem
+  real* elem_D;
   real* elem_Dinv;
   real* elem_fcor;
   real* elem_spheremp;
   real* elem_metdet;
+  real* elem_rmetdet;
   real* elem_sub_elem_mass_flux;
 
   // Members of elem%state
@@ -32,7 +54,8 @@ struct Arrays
 
 struct Constants
 {
-
+  real  rrearth;
+  real  eta_ave_w;
 };
 
 struct Control
@@ -42,7 +65,7 @@ struct Control
 
 struct Derivative
 {
-
+  real Dvv[np][np];
 };
 
 struct TestData
@@ -51,10 +74,13 @@ struct TestData
   Constants   constants;
   Control     control;
   Derivative  deriv;
+  HVCoord     hvcoord;
 };
 
 int init_test_data (TestData& data);
 
 int cleanup_data (TestData& data);
+
+} // Namespace Homme
 
 #endif // DATA_STRUCTURES_HPP
