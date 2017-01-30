deb39416a72e692a2044175de36dcde258be6ebd
Michael Deakin <mdeakin@sandia.gov>
2017-01-30 16:59:39 -0700

    Significant restructuring of globals to accomodate CUDA

--- compute_and_apply_rhs_test/cxx/kokkos_scratch/TestData.cpp
+++ compute_and_apply_rhs_test/cxx/kokkos_scratch/TestData.cpp
@@ -1,67 +1,48 @@
 #include "TestData.hpp"
 
-namespace TinMan
-{
+namespace TinMan {
 
-void Constants::init_data ()
-{
-  kappa        = 1.0;
-  Rwater_vapor = 1.0;
-  Rgas         = 10.0;
-  eta_ave_w    = 1.0;
+void Control::Control(int num_elems, int num_threads)
+    : thread_control("Thread control information", num_threads) {
+  constexpr const int elems_per_thread = num_elems / num_threads;
+  Kokkos::parallel_for(num_threads, KOKKOS_LAMBDA(int i) {
+    thread_control(i).nets = elems_per_thread * i;
+    thread_control(i).nete = elems_per_thread * (i + 1);
+    thread_control(i).n0 = 0;
+    thread_control(i).np1 = 1;
+    thread_control(i).nm1 = 2;
+    thread_control(i).qn0 = 0;
+    thread_control(i).dt2 = 1.0;
+  });
 }
 
-void Control::init_data (const int num_elems)
-{
-  nets = 0;
-  nete = num_elems;
-  n0 = 0;
-  np1 = 1;
-  nm1 = 2;
-  qn0 = 0;
-
-  Real dt2 = 1.0;
+void Derivative::Derivative() {
+  Kokkos::parallel_for(NP * NP, KOKKOS_LAMBDA(int idx) {
+    static constexpr const Real values[NP * NP] = {
+      -3.0000000000000000,  -0.80901699437494745,  0.30901699437494745,
+      -0.50000000000000000,  4.0450849718747373,   0.00000000000000000,
+      -1.11803398874989490,  1.54508497187473700, -1.5450849718747370,
+       1.11803398874989490,  0.00000000000000000, -4.04508497187473730,
+       0.5000000000000000,  -0.30901699437494745,  0.80901699437494745,
+       3.000000000000000000
+    };
+    const int i = idx / NP;
+    const int j = idx % NP;
+    Dvv_host(i, j) = values[idx];
+  });
 }
 
-void HVCoord::init_data ()
-{
-  ps0 = 1.0;
-
-  for (int i=0; i<NUM_LEV_P; ++i)
-  {
-    hyai[i] = 1.0;
-  }
-}
-
-void Derivative::init_data ()
-{
-  Real values[16] = { -3.0000000000000000, -0.80901699437494745,  0.30901699437494745, -0.50000000000000000,
-                       4.0450849718747373,  0.00000000000000000, -1.11803398874989490,  1.54508497187473700,
-                      -1.5450849718747370,  1.11803398874989490,  0.00000000000000000, -4.04508497187473730,
-                       0.5000000000000000, -0.30901699437494745,  0.80901699437494745, 3.000000000000000000  };
-  for (int i=0; i<NP; ++i)
-  {
-    for (int j=0; j<NP; ++j)
-    {
-      Dvv[i][j] = values[j*NP + i];
-    }
-  }
-}
-
-TestData::TestData (const int num_elems)
-{
-  m_constants.init_data();
+TestData::TestData(const int num_elems) {
   m_control.init_data(num_elems);
   m_hvcoord.init_data();
   m_deriv.init_data();
 }
 
-void TestData::update_time_levels()
-{
+void TestData::update_time_levels() {
   int tmp = m_control.np1;
   m_control.np1 = m_control.nm1;
   m_control.nm1 = m_control.n0;
-  m_control.n0  = tmp;
+  m_control.n0 = tmp;
 }
 
 } // Namespace TinMan
--- compute_and_apply_rhs_test/cxx/kokkos_scratch/TestData.hpp
+++ compute_and_apply_rhs_test/cxx/kokkos_scratch/TestData.hpp
@@ -8,43 +8,68 @@
 namespace TinMan
 {
 
+struct Constants
+{
+  static constexpr const Real rrearth = 1.0;
+  static constexpr const Real eta_ave_w = 1.0;
+  static constexpr const Real Rwater_vapor = 1.0;
+  static constexpr const Real Rgas = 10.0;
+  static constexpr const Real kappa = 1.0;
+};
+
 struct HVCoord
 {
   Real ps0;         // base state surface-pressure for level definitions
   Real hyai[NUM_LEV_P]; // ps0 component of hybrid coordinate - interfaces
-
-  void init_data ();
 };
 
-struct Constants
+class Control
 {
-  Real  rrearth;
-  Real  eta_ave_w;
-  Real  Rwater_vapor;
-  Real  Rgas;
-  Real  kappa;
+public:
+  // num_elems is te number of elements in the simulation
+  // num_workers is the number of threads used to simulate
+  // thread_id is between 0 and num_elems-1, inclusive
+  // thread_id should be unique between threads
+  Control(int num_elems, int num_threads);
 
-  void init_data ();
-};
+  int nets(int thread_id) const;
+  int nete(int thread_id) const;
+  int n0(int thread_id) const;
+  int np1(int thread_id) const;
+  int nm1(int thread_id) const;
+  int qn0(int thread_id) const;
+  int dt2(int thread_id) const;
+private:
+  struct Control_Data {
+    int  nets;
+    int  nete;
+    int  n0;
+    int  np1;
+    int  nm1;
+    int  qn0;
+    Real dt2;
+  };
 
-struct Control
-{
-  int  nets;
-  int  nete;
-  int  n0;
-  int  np1;
-  int  nm1;
-  int  qn0;
-  Real dt2;
-
-  void init_data (const int num_elems);
+  // Use this "struct" to ensure each Control_Data object fills a cache line
+  // This is necessary to prevent threads on different cores from causing false-sharing cache issues
+  template <typename cache_filler>
+  struct Cache_Wrapper : public cache_filler
+  {
+  private:
+    // x86 cache line size is 64
+    // CUDA cache line size may vary, but Fermi and Kepler are 128
+    constexpr const int cache_line_size = 128;
+    constexpr const int leftover_line = cache_line_size - (sizeof(cache_filler) % cache_line_size);
+    char filler[leftover_line];
+  };
+  ExecViewManaged<Cache_Wrapper<Control_Data> *> thread_control;
 };
 
 struct Derivative
 {
-  Real Dvv[NP][NP];
-
-  void init_data ();
+public:
+  Derivative();
+  ExecViewManaged<Real[NP][NP]> Dvv;
 };
 
 class TestData
@@ -52,7 +77,6 @@ class TestData
 public:
   TestData (const int num_elems);
 
-  const Constants&  constants() const { return m_constants; }
   const Control&    control()   const { return m_control;   }
   const Derivative& deriv()     const { return m_deriv;     }
   const HVCoord&    hvcoord()   const { return m_hvcoord;   }
@@ -60,7 +84,6 @@ public:
   void update_time_levels();
 
 private:
-  Constants   m_constants = {};
   Control     m_control   = {};
   Derivative  m_deriv     = {};
   HVCoord     m_hvcoord   = {};
